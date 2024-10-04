c20b2df4964eb6af6a04c64acc948a043e59b614
Cristian Loaiza Huallpa <49375434+cristianloa@users.noreply.github.com>
2024-10-04 15:50:07 -0500

    Update control-app.js

--- control-app.js
+++ control-app.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
         .then(function(reg) {
-            console.log('Service Worker registrado:', reg);
+            console.log('Service Worker registrado correctamente:', reg);
         })
         .catch(function(error) {
             console.error('Error al registrar el Service Worker:', error);
@@ -11,15 +11,18 @@ if ('serviceWorker' in navigator) {
     // Solicitar permiso para notificaciones
     Notification.requestPermission().then(function(result) {
         if (result === 'granted') {
-            console.log('Permiso concedido para notificaciones');
+            console.log('Permiso para notificaciones concedido');
         } else {
-            console.log('Permiso denegado para notificaciones');
+            console.log('Permiso para notificaciones denegado');
         }
     });
+} else {
+    console.error('Service Workers no son soportados por este navegador.');
 }
 
 // Manejar la recepción de notificaciones
 navigator.serviceWorker.addEventListener('message', function(event) {
+    console.log('Notificación recibida:', event.data);
     const status = document.querySelector('.status');
     const historyList = document.getElementById('history-list');
     status.textContent = "¡Notificación recibida!";
