4bab7c9a4881336e09f7c589e4030b91ce8f50c2
Cristian Loaiza Huallpa <49375434+cristianloa@users.noreply.github.com>
2024-10-04 15:15:59 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
 self.addEventListener('push', function(event) {
-    const data = event.data.json();
-    const title = data.title || 'Notificación de Sustentación';
     const options = {
-        body: data.body || 'Es momento de regresar a la sala de sustentación.',
-        icon: 'icon.png'  // Puedes incluir un ícono si lo deseas
+        body: event.data.text(), // Cuerpo del mensaje de la notificación
+        icon: 'icon.png' // Ruta del ícono (opcional)
     };
-
-    // Mostrar la notificación
+    
     event.waitUntil(
-        self.registration.showNotification(title, options)
+        self.registration.showNotification('Notificación de Sustentación', options)
     );
+});
+
+// Manejo de mensajes del cliente
+self.addEventListener('message', function(event) {
+    // Puedes manejar el mensaje que envía el cliente aquí si es necesario
 });
